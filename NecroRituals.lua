816c560ba62f1113dfefdb537850caf3d909e16e
higgins-dotcom <74015281+higgins-dotcom@users.noreply.github.com>
2024-05-11 09:11:36 +0100

    Update NecroRituals.lua

--- NecroRituals.lua
+++ NecroRituals.lua
@@ -4,7 +4,7 @@
 @description Peforms Rituals
 @author Higgins <discord@higginshax>
 @date 27/11/2023
-@version 2.5
+@version 2.6
 
 Disturbances handled
 0-300%
@@ -350,78 +350,10 @@ local function CheckForNewMessages()
     return false
 end
 
-
--- Rounds a number to the nearest integer or to a specified number of decimal places.
-local function round(val, decimal)
-    if decimal then
-        return math.floor((val * 10 ^ decimal) + 0.5) / (10 ^ decimal)
-    else
-        return math.floor(val + 0.5)
-    end
-end
-
-function formatNumber(num)
-    if num >= 1e6 then
-        return string.format("%.1fM", num / 1e6)
-    elseif num >= 1e3 then
-        return string.format("%.1fK", num / 1e3)
-    else
-        return tostring(num)
-    end
-end
-
--- Format script elapsed time to [hh:mm:ss]
-local function formatElapsedTime(startTime)
-    local currentTime = os.time()
-    local elapsedTime = currentTime - startTime
-    local hours = math.floor(elapsedTime / 3600)
-    local minutes = math.floor((elapsedTime % 3600) / 60)
-    local seconds = elapsedTime % 60
-    return string.format("[%02d:%02d:%02d]", hours, minutes, seconds)
-end
-
-local function calcProgressPercentage(skill, currentExp)
-    local currentLevel = API.XPLevelTable(API.GetSkillXP(skill))
-    if currentLevel == 120 then return 100 end
-    local nextLevelExp = XPForLevel(currentLevel + 1)
-    local currentLevelExp = XPForLevel(currentLevel)
-    local progressPercentage = (currentExp - currentLevelExp) / (nextLevelExp - currentLevelExp) * 100
-    return math.floor(progressPercentage)
-end
-
-local function printProgressReport(final)
-    local skill = "NECROMANCY"
-    local currentXp = API.GetSkillXP(skill)
-    local elapsedMinutes = (os.time() - startTime) / 60
-    local diffXp = math.abs(currentXp - startXp);
-    local xpPH = round((diffXp * 60) / elapsedMinutes);
-    local time = formatElapsedTime(startTime)
-    local currentLevel = API.XPLevelTable(API.GetSkillXP(skill))
-    IGP.radius = calcProgressPercentage(skill, API.GetSkillXP(skill)) / 100
-    IGP.string_value = time ..
-        " | " ..
-        string.lower(skill):gsub("^%l", string.upper) ..
-        ": " .. currentLevel .. " | XP/H: " .. formatNumber(xpPH) .. " | XP: " .. formatNumber(diffXp)
-end
-
-local function setupGUI()
-    IGP = API.CreateIG_answer()
-    IGP.box_start = FFPOINT.new(5, 5, 0)
-    IGP.box_name = "PROGRESSBAR"
-    IGP.colour = ImColor.new(116, 2, 179);
-    IGP.string_value = "NECROMANY RITUALS"
-end
-
-function drawGUI()
-    DrawProgressBar(IGP)
-end
-
-setupGUI()
 API.SetDrawTrackedSkills(true)
 
 while (API.Read_LoopyLoop()) do
     idleCheck()
-    drawGUI()
 
     API.DoRandomEvents()
 
@@ -480,6 +412,5 @@ while (API.Read_LoopyLoop()) do
     end
 
     ::continue::
-    printProgressReport()
     API.RandomSleep2(100, 200, 200)
 end
